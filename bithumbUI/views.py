992f9b26fa7aceff3600f84167c7b4a95b2b9147
HapH <jyoon0c11@gmail.com>
2022-08-18 20:15:46 +0900

    orderbook

--- bithumbUI/views.py
+++ bithumbUI/views.py
@@ -16,20 +16,63 @@ seckey = ""
 
 @csrf_exempt
 def index(request):
-    trade_price, opening_price, min_price, max_price, trade_volume = coin_info(f"https://api.bithumb.com/public/ticker/BTC_KRW")
-    context = {'trade_price':trade_price, 'opening_price':opening_price, 'min_price':min_price, 'max_price':max_price,'trade_volume':trade_volume}
-    return render(request, 'bithumbUI/bithumb.html', {'trade_price':trade_price, 'opening_price':opening_price, 'min_price':min_price, 'max_price':max_price,'trade_volume':trade_volume})
+    asks_price_ratio = []
+    bids_price_ratio = []
+
+    trade_price, opening_price, min_price, max_price, trade_volume \
+    = coin_info(f"https://api.bithumb.com/public/ticker/BTC_KRW")
+
+    asks_price, asks_quantity, bids_price, bids_quantity \
+        = orderbook(f"https://api.bithumb.com/public/orderbook/BTC_KRW")
+
+    for i in range(0, 30):
+        asks_price_ratio.append(((int(asks_price[i]) / int(opening_price)) - 1) * 100)
+    
+    for i in range(0, 30):
+        bids_price_ratio.append((int(bids_price[i]) / int(opening_price) - 1) * 100)
+    
+    return render(request, 'bithumbUI/bithumb.html', \
+    {'trade_price':trade_price, 'opening_price':opening_price, \
+    'min_price':min_price, 'max_price':max_price,'trade_volume':trade_volume, \
+    'asks_price': asks_price, 'asks_quantity' : asks_quantity,\
+    'bids_price': bids_price, 'bids_quantity': bids_quantity,\
+    'asks_price_ratio': asks_price_ratio, 'bids_price_ratio':bids_price_ratio})
 
 @csrf_exempt
 def orderbook(url):
-    response = requests.get(url)
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+    }
+
+    bids_price = []
+    bids_quantity = []
+    asks_price = []
+    asks_quantity = []
+    asks_price_ratio = []
+    bids_price_ratio = []
+
+    response = requests.get(url, headers=headers)
     response_json = response.json()
 
-    BTC_price = {}
+    for i in range(0, 30):
+        asks_price.append(response_json['data']['asks'][i]['price'])
 
-    for i in range(0, len(response_json)):
-        price = response_json[i]['price']
-    return BTC_price
+    for i in range(0, 30):
+        asks_quantity.append(response_json['data']['asks'][i]['quantity'])
+
+    for i in range(0, 30):
+        bids_price.append(response_json['data']['bids'][i]['price'])
+
+    for i in range(0, 30):
+        bids_quantity.append(response_json['data']['bids'][i]['quantity'])
+
+    asks_quantity.reverse()
+    asks_price.reverse()
+
+    return asks_price, asks_quantity, bids_price, bids_quantity
+    
+
 @csrf_exempt
 def coin_info(url):
     headers = {"Accept": "application/json"}
